8147da5b1c1d742a67f71cebc9af550509041961
ramin azizi <ray@hititup.com>
2016-07-24 23:49:00 -0700

    all initial views created for Recipes and Items

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,62 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+# GET /recipes
+  def index
+    @recipes = Recipe.all
+  end
+# GET /recipes/1.json
+  def show
+  end
+
+# GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
+# GET /recipes/1/edit
+  def edit
+  end
+# POST /recipes
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to @recipe, notice: 'Recip was successfully created.' }
+        format.json { render :show, status: :created, location: @recipe }
+      else
+        format.html { render :new }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+# PATCH/PUT /recipes/1
+  def update
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+# DELETE /recipes/1
+  def destroy
+    @recipe.destroy
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    def recipe_params
+      params.require(:recipe).permit(:name)
+    end
 end
